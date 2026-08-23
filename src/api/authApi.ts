3a850a99fb6363d0a81707070e40ecf3d2da8ff8
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-08-23 21:36:17 +0200

    setup auth api

---
+++ src/api/authApi.ts
@@ -0,0 +1,27 @@
+import { type User, type RegisterPayload } from "../types/users";
+import { encryptPassword } from "../utils/encryption";
+import { baseApi } from "./baseApi";
+
+export const authApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        findUserByEmail: builder.query<User[], string>({
+            query: (email) => `/users?email=${email}`,
+        }),
+        registeredUser: builder.mutation<User, RegisterPayload>({
+            query: (payload) => ({
+                url: "/users",
+                method: "POST",
+                body: {...payload, password: encryptPassword(payload.password) },
+            }),
+            invalidatesTags: ["Users"],
+        }),
+        updateUser: builder.mutation<User, {id: string; changes: Partial<User> }>({
+            query: ({id, changes }) => ({
+                url: `/users/${id}`,
+                method: "PATCH",
+                body: changes }),
+                invalidatesTags: ["Users"]
+        }),
+    }),
+})
+
