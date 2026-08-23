48879bb374822b77a7add0bb8f5ef3c0d7b37885
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-08-23 21:05:13 +0200

    setup hook

---
+++ src/app/hooks.ts
@@ -0,0 +1,5 @@
+import type { AppDispatch, RootState } from "./store";
+import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
