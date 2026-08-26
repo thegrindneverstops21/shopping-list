65b57f5d28dd64a68d7c87e95e6dcefa3e74c3ab
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-08-26 13:27:19 +0200

    wired the imports inside the main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./styles/auth.css"
+import "./styles/global.css"
+import "./styles/tokens.css"
+import "./styles/layout.css"
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
