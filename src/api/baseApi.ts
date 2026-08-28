fdd6d864cf7dee7498524877c324c1a9727720c2
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-08-28 08:46:47 +0200

    feat: add new shopping lists and items for user management

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001",
+    }),
     tagTypes: ["Users", "Lists", "Items"],
     endpoints: () => ({}),
 });
